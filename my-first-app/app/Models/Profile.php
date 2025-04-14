b7120651c8392fbd271112c12a39d3380dd1390f
PlamenPenkov123 <plamenpenkov29@gmail.com>
2025-04-14 14:03:18 +0300

    Profile model

--- my-first-app/app/Models/Profile.php
+++ my-first-app/app/Models/Profile.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "email",
+        "password",
+        "full_name",
+        "address",
+        "phone"
+    ];
+
+    public function setPasswordAttribute($pass) {
+        $this -> attributes["password"] = Hash::make($pass);
+    }
+    protected $hidden = [
+        'password'
+    ];
 }
